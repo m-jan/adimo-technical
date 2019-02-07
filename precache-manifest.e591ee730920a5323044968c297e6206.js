9cdc064a932920f47915d8dc8e267da5600c6169
Mohammed Jan <momo.a.jan@gmail.com>
2019-02-07 22:28:08 +0000

    Updates

---
+++ precache-manifest.e591ee730920a5323044968c297e6206.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bab3e28ea80c2e84b0",
+    "url": "/adimo-technical/static/js/runtime~main.18bab3e2.js"
+  },
+  {
+    "revision": "8faf6109efd7665ad8a8",
+    "url": "/adimo-technical/static/js/main.8faf6109.chunk.js"
+  },
+  {
+    "revision": "98f385cdfb4c4809a12a",
+    "url": "/adimo-technical/static/js/1.98f385cd.chunk.js"
+  },
+  {
+    "revision": "8faf6109efd7665ad8a8",
+    "url": "/adimo-technical/static/css/main.3d12d7cd.chunk.css"
+  },
+  {
+    "revision": "7cdc93349ed3c188d28a878857d60759",
+    "url": "/adimo-technical/index.html"
+  }
+];
